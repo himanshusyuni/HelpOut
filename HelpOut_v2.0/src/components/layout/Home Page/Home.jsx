cb66c209b1cc00527b7c77edb7ee5b511070690a
harshivg <ganwaniharshiv@gmail.com>
2024-02-01 22:09:29 +0530

    to fix: sign in form errors edited home page

--- HelpOut_v2.0/src/components/layout/Home Page/Home.jsx
+++ HelpOut_v2.0/src/components/layout/Home Page/Home.jsx
@@ -1,26 +1,34 @@
 import { useContext } from "react";
 import ActiveList from "../../../store/ActiveList";
 import RequestCard from "./RequestCard";
+import CreatePost from "./CreatePost";
+import Filter from "./Filters";
 
-function Home() {
+function Home({ setCurrPage }) {
   const List = useContext(ActiveList);
   return (
     <>
-      <div>
-        {List.map((items) => (
-          <RequestCard
-            key={items.Deadline}
-            Name={items.Name}
-            College={items.College}
-            Commision={items.Commision}
-            Help={items.Help}
-            Deadline={items.Deadline}
-            Profile={items.Picture}
-            Attachment={items.Attachment}
-            Description={items.Explain}
-            Contact={items.Contact}
-          />
-        ))}
+      <div className="pt-[100px] flex justify-between align-center border-2 border-red-500">
+        <div className="w-full border-2">
+          {List.map((items) => (
+            <RequestCard
+              key={items.Deadline}
+              Name={items.Name}
+              College={items.College}
+              Commision={items.Commision}
+              Help={items.Help}
+              Deadline={items.Deadline}
+              Profile={items.Picture}
+              Attachment={items.Attachment}
+              Description={items.Explain}
+              Contact={items.Contact}
+            />
+          ))}
+        </div>
+        <div className="bg-black text-center">
+          <CreatePost setCurrPage={setCurrPage} />
+          <Filter />
+        </div>
       </div>
     </>
   );
--- HelpOut_v2.0/src/components/layout/Home Page/RequestCard.jsx
+++ HelpOut_v2.0/src/components/layout/Home Page/RequestCard.jsx
@@ -1,28 +1,84 @@
-function RequestCard({Name,College,Help,Commision,Deadline,Profile,Attachment,Description,Contact}){
-    return(
-        <>
-        <div className="flex  w-full min-w-[760px]  rounded-lg mt-2 ml-10 justify-between border-2 border-black shadow-lg">
-            <div className="flex mt-2 ml-2">
-            <img src={Profile} alt="Profile" className="h-[100px] w-[100px]  rounded-full m-2 border-2 border-black " />
-            <div className="font-bold m-2 font-Lora" >
+import { Button } from "@/components/ui/button";
+import Container from "../AddHelp/container";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Info } from "lucide-react";
 
-                <p className=" text-red-900 text-[25px] text-nowrap">{Name}</p>
-                <p className="mt-[-7px]  text-blue-800 text-sm">from {College}</p>
-                <p className=" text-[25px] text-pretty">Needs {Help}</p>
-            </div>
-            </div>
-            <div className="m-8 text-center text-red-600 font-bold">
-                <p>DeadLine</p>
-                <p >{Deadline}</p>
-            </div>
-
-            <div className="m-2 font-semibold">
-                <p className="bg-blue-300 border-2  italic border-black rounded-md m-2 p-2">Commision : {Commision} rupees</p>
-                <button className="active:bg-green-400 text-purple-800 italic border-2 border-green-600 bg-slate-100 p-2  mt-[1px] ml-[50px] rounded-lg ">View Details</button>
-            </div>
-
-        </div>
-        </>
-    )
+function getFirstName(fullName) {
+  const names = fullName.split(" ");
+  return names[0];
 }
+
+function getDate(Deadline) {
+  const date = Deadline.split(" ");
+  return date[0];
+}
+
+function getTime(Deadline) {
+  const date = Deadline.split(" ");
+  return date[1];
+}
+
+function RequestCard({
+  Name,
+  College,
+  Help,
+  Commision,
+  Deadline,
+  Profile,
+  Attachment,
+  Description,
+  Contact,
+}) {
+  return (
+    <>
+      <div className="w-full max-w-[600px] mx-auto flex-1 flex items-center text-[100%]">
+        <div className="flex w-full rounded-md mt-2 border-2 border-black shadow-lg">
+          <div className="flex items-center m-5">
+            {/* Avatar */}
+            {/* <Button
+                variant="outline"
+                className="rounded-full hover:bg-white"
+              >
+                <Avatar>
+                  <AvatarImage
+                    src={Profile}
+                    alt="@shadcn"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </Button> */}
+            <img src={Profile} className="rounded-full w-[150px]" />
+          </div>
+
+          <div className="flex flex-col w-full p-2">
+            <div className="w-full">
+              <p className="font-medium text-purple-500 text-3xl">
+                {getFirstName(Name)}
+              </p>
+              <p className="text-blue-800 text-lg">from {College}</p>
+              <p className="text-pretty text-2xl">Needs {Help}</p>
+            </div>
+
+            <div className="w-full flex flex-col items-center p-1 text-red-500 font-base">
+              <p className="text-xl">
+                By {getTime(Deadline)} {getDate(Deadline)}
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center gap-4 pl-4 pr-4 font-semibold">
+            <Button className="w-full bg-blue-500 hover:bg-blue-500 cursor-auto text-lg p-5">
+              Commission: ₹ {Commision}
+            </Button>
+            <Button className="w-full bg-green-500 hover:bg-purple-500 text-lg p-5">
+              View Details
+              <Info className="pl-3 size-8" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
 export default RequestCard;
